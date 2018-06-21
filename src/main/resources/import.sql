bfcd4c9aa142e2e73aa848ac35057442020581d6
Robert12222 <robert.james14@googlemail.com>
2018-06-21 10:42:00 +0100

    First

---
+++ src/main/resources/import.sql
@@ -0,0 +1,3 @@
+INSERT INTO BOOK (id, bookTitle,genre, yearPublished) VALUES (1, 'A Game of Thrones', 'Epic fantasy', '1996')
+INSERT INTO BOOK (id, bookTitle,genre, yearPublished) VALUES (1, 'Dark Reign', 'Superhero Crossover', '2009')
+INSERT INTO BOOK (id, bookTitle,genre, yearPublished) VALUES (1, 'The Lordd Of The Rings', 'Fantasy Adventure', '1954')
